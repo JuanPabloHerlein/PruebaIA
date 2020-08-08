cecd0058f51b5749f91a2bd1108d8d61949b1677
Augusto Herbel <augusto.h@geopagos.com>
2020-08-08 20:36:43 -0300

    Code reformat

--- app/src/main/java/com/juanpablo/pruebaiabis/MainActivity.java
+++ app/src/main/java/com/juanpablo/pruebaiabis/MainActivity.java
@@ -1,47 +1,52 @@
 package com.juanpablo.pruebaiabis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView salida;
-    String []CADENA;
+    AppCompatTextView salida;
+    AppCompatEditText input;
+    AppCompatButton btnSend;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        salida=(TextView)findViewById(R.id.Tv1);
-        CADENA=separaCaracteres("hola juan$ pablo, ¿como estas?.","[,. ¿?]+");
-        //salida.setText(""+CADENA);
-        salida.setText(mostrar(CADENA));
+        salida = (AppCompatTextView) findViewById(R.id.Tv1);
+        input = (AppCompatEditText) findViewById(R.id.input);
+        btnSend = (AppCompatButton) findViewById(R.id.btnSend);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String text = input.getText().toString();
+                final String[] result = separaCaracteres(text);
+                salida.setText(mostrar(result));
+            }
+        });
     }
 
-
-    public String mostrar(String []cadena){
-        String r="";
-        for(int i=0;i<cadena.length;i++){
-            r += ("\n---\npalabra: " + i +">>>"+ cadena[i]);
+    public String mostrar(String[] cadena) {
+        final StringBuilder result = new StringBuilder();
+        for (int i = 0; i < cadena.length; i++) {
+            result.append("\n---\npalabra: ");
+            result.append(i);
+            result.append(">>>");
+            result.append(cadena[i]);
         }
-        return r;
+        return result.toString();
     }
 
-
-    public String[] separaCaracteres(String cadena, String separator){
-        //System.out.println("Separator: " + separator);
-        String[] parts = null;
-        if(separator.equals("|")|| separator.equals("\\")||separator.equals(".")||separator.equals("^")||separator.equals("$")
-                ||separator.equals("?")||separator.equals("*")||separator.equals("+")||separator.equals("(")||separator.equals(")")
-                ||separator.equals("{")||separator.equals("[")||separator.equals(" ")){
-            //Es metacaracter!
-            parts = cadena.split(" "+separator);
-        }else{
-            //No es metacaracter.
-            parts = cadena.split(separator);
-        }
-        return parts;
+    public String[] separaCaracteres(String cadena) {
+        final String separators = "[,. ¿?$]+";
+        return cadena.split(separators);
     }
 }
